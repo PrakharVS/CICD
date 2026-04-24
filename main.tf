bb8108e6b607bb854482c66104274b9eb7dc3d4a
Prakhar <prakharvikramsingh811@hotmail.com>
2026-04-24 12:41:55 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-variable "ec2_ip" {}
-
 resource "null_resource" "deploy" {
+
+  triggers = {
+    always_run = timestamp()
+  }
 
   connection {
     type        = "ssh"
@@ -15,26 +13,15 @@ resource "null_resource" "deploy" {
 
   provisioner "remote-exec" {
     inline = [
-
-      # Install Docker (Ubuntu)
       "sudo apt update -y",
       "sudo apt install docker.io -y",
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
-
-      # Add user to docker group
       "sudo usermod -aG docker ubuntu",
-
-      # Stop and remove old container
       "sudo docker stop portfolio || true",
       "sudo docker rm portfolio || true",
-
-      # Pull latest image
       "sudo docker pull prakharvs/webserver:latest",
-
-      # Run container
       "sudo docker run -d -p 80:80 --name portfolio prakharvs/webserver:latest"
     ]
   }
 }
-
